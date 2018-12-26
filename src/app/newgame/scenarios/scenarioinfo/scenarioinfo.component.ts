d465097854cc34b35cab61642746a9fb74234edf
Alexey Karpechenko <karpechenko.lesha@gmail.com>
2018-12-27 01:34:31 +0300

    3/4 of changes

--- src/app/newgame/scenarios/scenarioinfo/scenarioinfo.component.ts
+++ src/app/newgame/scenarios/scenarioinfo/scenarioinfo.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-scenarioinfo',
   templateUrl: './scenarioinfo.component.html',
-  styleUrls: ['./scenarioinfo.component.css']
+  styleUrls: ['./scenarioinfo.component.css', '../../../../../node_modules/bootstrap/dist/css/bootstrap.min.css']
 })
 export class ScenarioinfoComponent implements OnInit {
 
