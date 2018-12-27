27fe529ed2ca7d82d3e821a26df78adc84336542
Alexey Karpechenko <karpechenko.lesha@gmail.com>
2018-12-27 18:36:13 +0300

    3.9/4 of changes

--- src/app/newgame/scenarios/newscenario/newscenario.component.ts
+++ src/app/newgame/scenarios/newscenario/newscenario.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-newscenario',
   templateUrl: './newscenario.component.html',
-  styleUrls: ['./newscenario.component.css']
+  styleUrls: ['./newscenario.component.css',  '../../../../../node_modules/bootstrap/dist/css/bootstrap.min.css']
 })
 export class NewscenarioComponent implements OnInit {
 
